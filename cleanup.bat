6f47ca02495d791e1a9a30b6d4ea852e08ecfd95
delta-12 <40147170+delta-12@users.noreply.github.com>
2018-08-20 23:03:56 -0400

    Create cleanup.bat

---
+++ cleanup.bat
@@ -0,0 +1,5 @@
+cd %userprofile%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup
+powershell del svchost.lnk
+cd %userprofile%\AppData\Local
+powershell del svchost.bat
+powershell del svchost.vbs
